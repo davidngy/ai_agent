ca43ed3f5ee307e26731497943dc9246affeb08d
davidngy <davidngy@gmail.com>
2025-06-20 17:58:49 +0200

    conversation messages saved

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+import sys
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
+    if len(sys.argv) < 2:
+        print("Prompt missing")
+        sys.exit(1)
+    
+    user_prompt = sys.argv[1]
+    
 
-print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-print("Response tokens:", response.usage_metadata.candidates_token_count)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents=messages
+    )
+    if len(sys.argv) == 3:
+        if str(sys.argv[2]) == "--verbose":
+            print(f"User prompt: {user_prompt}")
+            print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+            print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
