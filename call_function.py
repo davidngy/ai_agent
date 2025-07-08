134616134842920e76c3e2d4f1f0894324651273
davidngy <davidngy@gmail.com>
2025-07-08 22:03:03 +0200

    finish the base for my first ai agent

---
+++ call_function.py
@@ -0,0 +1,55 @@
+from google.genai import types
+
+from functions.get_files_info import schema_get_files_info, get_files_info
+from functions.get_file_content import schema_get_file_content, get_file_content
+from functions.run_python_file import schema_run_python_file, run_python_file
+from functions.write_file import schema_write_file, write_file
+
+def call_function(function_call_part, verbose=False):
+    if verbose == True:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+    
+    function_map= {
+        "get_files_info": get_files_info,
+        "get_file_content": get_file_content,
+        "run_python_file": run_python_file,
+        "write_file": write_file     
+    }
+
+    function_name = function_call_part.name
+    
+    if function_name not in function_map:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_call_part.name,
+                    response={"error": f"Unknown function: {function_name}"},
+                )
+            ],
+        )
+    
+    args = function_call_part.args.copy()
+    args["working_directory"] = "./calculator"
+    function_result = function_map[function_name](**args)
+
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_call_part.name,
+                response={"result": function_result},
+            )
+        ],
+    )
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file
+    ]
+)
--- main.py
+++ main.py
@@ -1,36 +1,87 @@
 import os
-from dotenv import load_dotenv
+import sys
+
 from google import genai
 from google.genai import types
-from tests import *
-import sys
+from dotenv import load_dotenv
+
+from prompts import system_prompt
+from call_function import available_functions, call_function
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    verbose = "--verbose" in sys.argv
 
     if len(sys.argv) < 2:
         print("Prompt missing")
         sys.exit(1)
-    
+
     user_prompt = sys.argv[1]
     
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
+    
+    for count in range(0, 21):
+        if count == 20:
+            print(f"Maximum iterations (20) reached.")
+            sys.exit(1)
+            break
+        try:
+            response = generate_content(client, messages, verbose)
+            if response:
+                print(response)
+                break
+        except Exception as e:
+            print(f"Error in generate_content: {e}")
+
+def generate_content(client, messages, verbose):
+   
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=messages
+        model='gemini-2.0-flash-001', 
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        )
     )
     
-    if len(sys.argv) == 3:
-        if str(sys.argv[2]) == "--verbose":
-            print(f"User prompt: {user_prompt}")
-            print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-            print("Response tokens:", response.usage_metadata.candidates_token_count)
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+    
+    if response.candidates:
+        for candidate in response.candidates:
+            messages.append(candidate.content)
 
-    print(response.text)
+    if not response.function_calls:
+        return response.text
+    
+
+    function_responses = []
+
+    
+    for function_call_part in response.function_calls:
+        function_call_result = call_function(function_call_part, verbose)
+
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("empty function call result")
+        
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+
+        function_responses.append(function_call_result.parts[0])
+  
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
+
+    messages.append(types.Content(role="tool", parts=function_responses))    
 
 
-if __name__ == "__main__":
+
+if __name__ == "__main__": 
     main()
